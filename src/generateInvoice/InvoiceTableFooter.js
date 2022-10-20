6801e6a64555911d91063e2032bf38f396ad1f8f
Slahudeen <slahudeen@remotepass.com>
2022-10-20 15:22:05 +0500

    increased the size of bank details font

--- src/generateInvoice/InvoiceTableFooter.js
+++ src/generateInvoice/InvoiceTableFooter.js
@@ -30,7 +30,7 @@ const InvoiceTableFooter = ({ items }) => {
     const total = items.map(item => item.qty * item.rate)
         .reduce((accumulator, currentValue) => accumulator + currentValue, 0)
     return (
-        <View style={styles.row}>
+        <View >
              {/* <Text style={styles.description}>Gesamtbetrag</Text>
             <Text style={styles.total}>{Number.parseFloat(total).toFixed(2)}</Text> */} 
         </View>
--- src/generateInvoice/InvoiceThankyouMsg.js
+++ src/generateInvoice/InvoiceThankyouMsg.js
@@ -28,7 +28,7 @@ const styles = StyleSheet.create({
         marginTop: 12
     },
     reportText: {
-        fontSize: 9,
+        fontSize: 10,
         textAlign: 'left',
         textTransform: 'uppercase',
     },
@@ -44,7 +44,7 @@ const InvoiceThankYouMsg = () => (
         <Text style={styles.reportText}>Bankverbindung: SPARKASSE RHEIN NECKAR NORD </Text>
         <Text style={styles.reportText}>IBAN: DE06 6705 0505 0040 2145 42 </Text>
         <Text style={styles.reportText}>BIC: MANSDE66XXX</Text>
-        {/* <Text style={styles.reportTitle}>*** Vielen Dank ***</Text> */}
+        <Text style={styles.reportTitle}>*** Vielen Dank ***</Text>
         
         </View>
         <View fixed style={styles.titleContainer3}>
