234e3e66601b02f08a1abb4c9203efc2d2ef5f7d
Slahudeen <slahudeen@remotepass.com>
2022-11-15 15:11:34 +0500

    updation

--- src/generateInvoice/BillTo.js
+++ src/generateInvoice/BillTo.js
@@ -4,7 +4,7 @@ import { Text, View, StyleSheet } from '@react-pdf/renderer';
 const styles = StyleSheet.create({
     headerContainer: {
         marginTop: -25,
-        marginBottom: 10,
+        marginBottom: 11,
         justifyContent: 'flex-start',
         width: '70%',
         fontSize: 12,
